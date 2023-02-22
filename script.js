9bd3ddc9d2daa4bcfdc1095e5f06756de569a09b
Kamran Aghayev <aghayev.kamran@outlook.com>
2023-02-22 05:00:13 +0400

    0.1-JS

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"
+//speed from kmh to kms - 1st method
+
+let kmhSpeed = 360;
+console.log (kmhSpeed / 3600);
+
+// second method
+
+let kmhSpeed2 = 360;
+let msSpeed2 = kmhSpeed / 3600;
+console.log(msSpeed2 );
