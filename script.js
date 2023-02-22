6ed122badb186173e82f37a84e1e7ead91d8554a
Kamran Aghayev <aghayev.kamran@outlook.com>
2023-02-22 23:54:01 +0400

    0.3

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-"use strict"
+
 //speed from kmh to kms - 1st method
 
 let kmhSpeed = 360;
-console.log (kmhSpeed / 3600);
+console.warn (kmhSpeed / 3600);
 
 // second method
 
 let kmhSpeed2 = 360;
 let msSpeed2 = kmhSpeed / 3600;
-console.log(msSpeed2 );
+console.log(msSpeed2);
+
+//third method
+let kmhSpeed3 = 360;
+let htos = 3600;
+console.log(kmhSpeed3 / htos)
+
+
+let mytime = Date(1992,3,5)
+console.log(mytime)
+
+//bonus?
+
+// let today = Date();
+// let bday = '03/05/1992';
+
+// let x = new Date(today);
+// let y = new Date(bday);
+
+
+// const diffDays = Math.floor((x - y) / (1000 * 60 * 60 * 24));
+// console.log(diffDays);
+
+//second version of bonus
+let birthDate, today, days;
+birthDate = prompt("Please input your Birthday like yyyy,mm,dd " )
+today = new Date();
+birthDate = new Date(birthDate);
+days = Math.round((today - birthDate) / (1000 * 60 * 60 * 24));
+console.log(days)
+alert(` I did it:) and since your birthday has passed ${days} days. Have a good day!`);
